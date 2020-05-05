a48a1dda948f65930db52fe64ab3ba9f4d28b143
Squishyclaws <Squishycrawford1@gmail.com>
2020-05-05 16:44:52 +0200

    FirstUpload
    FirstUpload

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Enemy Stats")]
+    [SerializeField] float health = 100;
+    [SerializeField] int scoreValue = 150;
+
+
+
+    [Header("Shooting")]
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed = 10f;
+
+
+    [Header("Sound effects")]
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float DurationOfexplosion = 1f;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] float deathSoundVolume = 0.7f;
+    [SerializeField] AudioClip hitSound;
+    [SerializeField] float hitSoundVolume;
+    [SerializeField] AudioClip fireSound;
+    [SerializeField] float fireSoundVolume;
+
+    void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+
+        shotCounter -= Time.deltaTime;
+
+        if(shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+
+
+    }
+
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(
+           projectile,
+           transform.position,
+           Quaternion.identity)
+            as GameObject;
+        AudioSource.PlayClipAtPoint(fireSound, Camera.main.transform.position, fireSoundVolume);
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+
+    }
+    IEnumerator ChangeColor()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        yield return new WaitForSeconds(0.02f);
+        this.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        AudioSource.PlayClipAtPoint(hitSound, Camera.main.transform.position, hitSoundVolume);
+        Debug.Log(other.name + " has hit");
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
+        ProcessHit(damageDealer);
+        StartCoroutine(ChangeColor());
+    }
+ 
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<GameSession>().AddToScore(scoreValue);
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(explosion, DurationOfexplosion);
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
+    }
+}
---
+++ Assets/Script/Life.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    [SerializeField] AudioClip loseLife;
+    [SerializeField] float loseLifeVolume;
+    [SerializeField] public GameObject[] hearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void MinusLife5(int heartnumber)
+    {
+
+        hearts[heartnumber].SetActive(false);
+      
+    }
+
+
+
+
+}
