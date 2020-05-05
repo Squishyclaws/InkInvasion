7d616dabbf98607fba08fa9229b947f5cdd3ecc1
VedowCrain <Calvinvdwesthuizen@gmail.com>
2020-05-05 21:15:41 +0200

    Heart Animation
    Created Heart animation fixed partical bug

--- Assets/Calvin/Scripts/Enemy.cs
+++ Assets/Calvin/Scripts/Enemy.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
-    [Header("Enemy Stats")]
-    [SerializeField] float health = 100;
+    [Header("Enemy Stats")] [SerializeField]
+    float health = 100;
+
     [SerializeField] int scoreValue = 150;
 
 
-
-    [Header("Shooting")]
-    [SerializeField] float shotCounter;
+    [Header("Shooting")] [SerializeField] float shotCounter;
     [SerializeField] float minTimeBetweenShots = 0.2f;
     [SerializeField] float maxTimeBetweenShots = 3f;
     [SerializeField] GameObject projectile;
     [SerializeField] float projectileSpeed = 10f;
 
 
-    [Header("Sound effects")]
-    [SerializeField] GameObject deathVFX;
+    [Header("Sound effects")] [SerializeField]
+    GameObject deathVFX;
+
     [SerializeField] float DurationOfexplosion = 1f;
     [SerializeField] AudioClip deathSound;
     [SerializeField] float deathSoundVolume = 0.7f;
@@ -42,46 +42,48 @@ public class Enemy : MonoBehaviour
 
     private void CountDownAndShoot()
     {
-
         shotCounter -= Time.deltaTime;
 
-        if(shotCounter <= 0f)
+        if (shotCounter <= 0f)
         {
             Fire();
             shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
         }
-
-
     }
 
 
     private void Fire()
     {
         GameObject laser = Instantiate(
-           projectile,
-           transform.position,
-           Quaternion.identity)
+                projectile,
+                transform.position,
+                Quaternion.identity)
             as GameObject;
         AudioSource.PlayClipAtPoint(fireSound, Camera.main.transform.position, fireSoundVolume);
         laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
-
     }
+
     IEnumerator ChangeColor()
     {
         this.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
         yield return new WaitForSeconds(0.02f);
         this.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         AudioSource.PlayClipAtPoint(hitSound, Camera.main.transform.position, hitSoundVolume);
         Debug.Log(other.name + " has hit");
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
-        if (!damageDealer) { return; }
+        if (!damageDealer)
+        {
+            return;
+        }
+
         ProcessHit(damageDealer);
         StartCoroutine(ChangeColor());
     }
- 
+
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.GetDamage();
@@ -89,7 +91,6 @@ public class Enemy : MonoBehaviour
         if (health <= 0)
         {
             Die();
-
         }
     }
 
@@ -97,7 +98,7 @@ public class Enemy : MonoBehaviour
     {
         FindObjectOfType<GameSession>().AddToScore(scoreValue);
         Destroy(gameObject);
-        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        GameObject explosion = Instantiate(deathVFX, transform.position, Quaternion.identity);
         Destroy(explosion, DurationOfexplosion);
         AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
     }
