9ec205186c078b84fb1e16ed48c5525ef1867895
VedowCrain <Calvinvdwesthuizen@gmail.com>
2020-05-05 20:22:05 +0200

    UI Update
    Updated all UI elements and partical systems have been enabled

---
+++ Assets/Calvin/Scripts/Singletons/PersistentSingleton.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// A <see cref="Singleton{T}"/> that persists between scenes
+/// </summary>
+/// <typeparam name="T">The type of Singleton GameObject</typeparam>
+public abstract class PersistentSingleton<T> : Singleton<T> where T : Singleton<T>
+{
+	/// <summary>
+	/// Set the <see cref="T"/> to not destroy on scene loads.
+	/// </summary>
+	protected override void Awake()
+	{
+		base.Awake();
+		DontDestroyOnLoad(gameObject);
+	}
+}
---
+++ Assets/Calvin/Scripts/Singletons/Singleton.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+/// <summary>
+/// A <see cref="MonoBehaviour"/> implementation of the Singleton Pattern
+/// </summary>
+/// <typeparam name="T">The type of the Singleton</typeparam>
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+	/// <summary>
+	/// The instance of <see cref="T"/>
+	/// </summary>
+	public static T Instance { get; protected set; }
+
+	/// <summary>
+	/// Is there an existing instance of <see cref="T"/> in the scene?
+	/// </summary>
+	public static bool InstanceExists
+	{
+		get { return Instance != null; }
+	}
+
+	/// <summary>
+	/// Assign the <see cref="Instance"/> and ensure there is only 1 of <see cref="T"/> in the scene
+	/// </summary>
+	protected virtual void Awake()
+	{
+		if(Instance != null)
+		{
+			//Debug.LogWarningFormat("There is another instance of <color=red>{0}</color>. There can only be one!", typeof(T));
+			Destroy(gameObject);
+		}
+		else
+		{
+			Instance = (T)this;
+			//print("Generate singleton: " + (T)this);
+		}
+	}
+
+	/// <summary>
+	/// Safely unassign <see cref="Instance"/>
+	/// </summary>
+	protected virtual void OnDestroy()
+	{
+		if(Instance == this)
+		{
+			Instance = null;
+		}
+	}
+}
